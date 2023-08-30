c82c21a8f676627030ff59a6b51c817a6a0467ef
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2023-08-30 17:32:31 -0300

    projeto_revisao_java

--- SistemasClienteServidor/prj_aula_02/src/Exercicio01/Main.java
+++ SistemasClienteServidor/prj_aula_02/src/Exercicio01/Main.java
@@ -3,5 +3,11 @@ package Exercicio01;
 public class Main {
     public static void main(String[] args) {
 
+    Usuario user = new Usuario("Bob","123456");
+
+    user.formatoSystemOut();
+
+
+
     }
 }
