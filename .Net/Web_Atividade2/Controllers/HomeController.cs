d6276a0378dea7476e72e5127d067992c77393e3
Gabriel Melo <115416454+GabriielMelo@users.noreply.github.com>
2023-10-29 10:08:51 -0300

    add

---
+++ .Net/Web_Atividade2/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web_Atividade2.Models;
+
+namespace Web_Atividade2.Controllers
+{
+    public class HomeController : Controller
+    {
+            
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Pagina1()
+        {
+            return View();
+        }
+        public IActionResult Pagina2()
+        {
+
+            return View();
+        }
+
+        public IActionResult Pagina3()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
